80842b22138a8f42db067bb9fcf54c0c456c43a6
Casey <casey@vm-ubuntu-12.(none)>
2013-06-02 21:50:59 +0200

    Package, scripts and install_requires fix

--- setup.py
+++ setup.py
@@ -5,13 +5,12 @@ setup(
     version='0.1.0',
     author='Martin Maga',
     author_email='xmagam00@stud.fit.vutbr.cz',
-    packages=['dist'],
-    scripts=['scholar/scholar.py'],
+    packages=['scholar'],
     url='https://github.com/KNOT-GIT/mGoogleScholar',
     license='LICENSE.txt',
     description='Python API module for scholar.gooogle.cz',
     long_description=open('README.md').read(),
     install_requires=[
-        "Beautiful Soup 4.0.1",
+        "beautifulsoup4 >= 4.0.1",
     ],
 )
